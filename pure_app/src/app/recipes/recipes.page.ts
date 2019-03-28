1853b3111c0c5cb5ba4a0dc3055eea68f44e474e
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-28 19:53:15 +0530

    Life Cycle Method Implemented for reFetch Data

--- pure_app/src/app/recipes/recipes.page.ts
+++ pure_app/src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewChecked} from '@angular/core';
 import {RecipesService} from './recipes.service';
 import {Recipe} from './recipe.model';
 
@@ -7,12 +7,16 @@ import {Recipe} from './recipe.model';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, AfterContentChecked {
   recipes: Recipe[];
 
   constructor(private recipesService: RecipesService) {}
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
+  }
+
+  ngAfterContentChecked() {
     this.recipes = this.recipesService.getAllRecipes();
   }
 }
