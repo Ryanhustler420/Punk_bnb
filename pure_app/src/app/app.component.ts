0660eb2105b407cdf7cf30277695bae8a7896255
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-29 13:16:46 +0530

    Adding Links & Switching Pages

--- pure_app/src/app/app.component.ts
+++ pure_app/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
   constructor(
@@ -22,5 +22,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  onLogout() {
+    console.log('Loggin off');
   }
 }
