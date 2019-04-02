7aea770a792c3fae8ba9cc2a8d31f886917a56c1
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-02 13:29:51 +0530

    Deleting Bookings

--- pure_app/src/app/bookings/booking.service.ts
+++ pure_app/src/app/bookings/booking.service.ts
@@ -75,13 +75,15 @@ export class BookingService {
   }
 
   cancelBooking(bookingId: string) {
-    return this.bookings.pipe(
-      take(1),
-      delay(1000),
-      tap(bookings => {
-        this._bookings.next(bookings.filter(b => b.id !== bookingId));
-      })
-    );
+    return this.http
+      .delete(`https://ionicpunkbnb.firebaseio.com/bookings/${bookingId}.json`)
+      .pipe(
+        switchMap(() => this.bookings),
+        take(1),
+        tap(bookings => {
+          this._bookings.next(bookings.filter(b => b.id !== bookingId));
+        })
+      );
   }
 
   fetchBookings() {
