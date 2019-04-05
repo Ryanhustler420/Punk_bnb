6b05a6b89c43d1429a2fa0acdd8901370e1fae4b
Gaurav Gupta <gouravgupta840@gmail.com>
2019-04-05 05:55:28 +0530

    Get Current Location Map

--- pure_app/src/app/shared/map-modal/map-modal.component.ts
+++ pure_app/src/app/shared/map-modal/map-modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
+import * as map from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-modal',
@@ -7,15 +8,33 @@ import {ModalController} from '@ionic/angular';
   styleUrls: ['./map-modal.component.scss'],
 })
 export class MapModalComponent implements OnInit {
-  lat = '40.7609177';
-  lng = '-73.9896704';
+  lat;
+  lng;
+  input: string;
 
   constructor(private modalCtrl: ModalController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+    // initial get the lat long for the current location
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(pos => {
+        this.lat = pos.coords.latitude + 2;
+        this.lng = pos.coords.longitude + 1;
+        console.log(this.lat, this.lng);
+      });
+    }
+  }
 
   onCancel() {
     // console.log('close');
     this.modalCtrl.dismiss();
+  }
+
+  run() {
+    if (!this.input) {
+      return;
+    }
+    console.log(this.input);
   }
 }
--- pure_app/src/app/shared/shared.module.ts
+++ pure_app/src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IonicModule} from '@ionic/angular';
+import {FormsModule} from '@angular/forms';
 
 import {LocationPickerComponent} from './pickers/location-picker/location-picker.component';
 import {MapModalComponent} from './map-modal/map-modal.component';
-import { MapBoxComponent } from '../map/map-box/map-box.component';
+import {MapBoxComponent} from '../map/map-box/map-box.component';
 
 @NgModule({
   declarations: [LocationPickerComponent, MapModalComponent, MapBoxComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, FormsModule],
   exports: [LocationPickerComponent, MapModalComponent, MapBoxComponent],
   entryComponents: [MapModalComponent],
 })
